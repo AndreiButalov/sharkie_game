8fe0fa7f56df5481e57dd531843e835e1d7af14b
AndrejYaotsin <andrej.butalov@gmx.de>
2024-07-03 14:40:27 +0200

    characterAnimate/EnemyAnimate

--- models/bubblefish.class.js
+++ models/bubblefish.class.js
@@ -15,7 +15,8 @@ class Bubblefish extends MovableObject {
     constructor() {
         super().loadImage('img/2.Enemy/1.Puffer fish (3 color options)/1.Swim/1.swim1.png');
         this.loadImages(this.BUBBLEFISH_SWIM);
-        this.x = 200 + Math.random() * 370;
+        // this.x = 200 + Math.random() * 370;
+        this.x = 720;
         this.y = 60 + Math.random() * 300;
         this.speed = 0.3 + Math.random() * 0.5;
         this.animateEnemy();
