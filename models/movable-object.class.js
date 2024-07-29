f5f9946e0af9c751c0a3768d5cc720d0e8c866e9
AndrejYaotsin <andrej.butalov@gmx.de>
2024-07-29 11:56:50 +0200

    characterDead/EndergyFunction

--- models/movable-object.class.js
+++ models/movable-object.class.js
@@ -1,36 +1,9 @@
-class MovableObject {
-    x = 0;
-    y = 230;
-    height = 200;
-    width = 200;
-    img;
-    currentImage = 0;
+class MovableObject extends DrawableObject {
+    
     speed = 0.2;
     otherDirection = false;
     energy = 100;
     lastHit = 0;
-
-    imageCache = {};
-
-    loadImage(path) {
-        this.img = new Image();
-        this.img.src = path;
-    }
-
-    draw(ctx) {
-        ctx.drawImage(this.img, this.x, this.y, this.width, this.height);
-    }
-
-
-    drawFrame(ctx) {
-        if (this instanceof Character || this instanceof Bubblefish) {
-            ctx.beginPath();
-            ctx.lineWidth = '6';
-            ctx.strokeStyle = 'red';
-            ctx.rect(this.x, this.y, this.width, this.height);
-            ctx.stroke();
-        }
-    }
 
 
     isColliding(obj) {
@@ -62,32 +35,28 @@ class MovableObject {
         return timepassed < 0.5;
     }
 
-
-    loadImages(arr) {
-        arr.forEach(path => {
-            let img = new Image();
-            img.src = path;
-            this.imageCache[path] = img;
-        });
-    }
-
+    
     moveRight() {
         this.x += this.speed;
         this.otherDirection = false;
     }
+    
 
     moveLeft() {
         this.x -= this.speed;
 
     }
 
+
     moveUp() {
         this.y -= this.speed;
     }
 
+
     moveDown() {
         this.y += this.speed;
     }
+
 
     playAnimation(images) {
         let i = this.currentImage % images.length;
--- models/world.class.js
+++ models/world.class.js
@@ -6,6 +6,7 @@ class World {
     ctx;
     keyboard;
     camera_x = 0;
+    statusBar = new StatusBar();
 
     constructor(canvas, keyboard) {
         this.ctx = canvas.getContext('2d');
@@ -22,7 +23,7 @@ class World {
             this.level.enemies.forEach((enemy) => {
                 if(this.character.isColliding(enemy)) {
                     this.character.hit();
-                    console.log(this.character.energy);
+                    this.statusBar.setPercentage(this.character.energy);
                 }
             })
         }, 200);
@@ -38,6 +39,11 @@ class World {
         this.ctx.translate(this.camera_x, 0);
 
         this.addObjectsToMap(this.level.backgroundObjects);
+        
+        this.ctx.translate(-this.camera_x, 0);
+        this.addToMap(this.statusBar);
+        this.ctx.translate(this.camera_x, 0);
+
         this.addToMap(this.character);
         this.addObjectsToMap(this.level.enemies);
         this.ctx.translate(-this.camera_x, 0);
