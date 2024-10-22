7e5be711cd8cc163d932797424afba7138f914a8
AndrejYaotsin <andrej.butalov@gmx.de>
2024-10-22 09:30:51 +0200

    JDOC/World/Game.js

--- js/game.js
+++ js/game.js
@@ -4,6 +4,9 @@ let keyboard = new Keyboard();
 let isFullScreen = false;
 
 
+/**
+ * Initializes the game by rendering the UI, setting up the pause button, sound button, and fullscreen functionality.
+ */
 function init() {
     renderUI();
     setupPauseButton();
@@ -12,6 +15,10 @@ function init() {
 }
 
 
+/**
+ * Renders various UI components by setting the inner HTML of specific elements.
+ * Calls different functions to generate content for each element.
+ */
 function renderUI() {
     const elementsToRender = [
         { id: 'start_view', content: startViewGenerate() },
@@ -27,14 +34,19 @@ function renderUI() {
 }
 
 
+/**
+ * Handles the fullscreen functionality by toggling between full screen and normal screen modes.
+ * Adds event listeners to the fullscreen button for click and keydown actions.
+ */
 function fullScreen() {
     let bntFullScreen = document.getElementById('btn_fullScreen');
     let fullscreen = document.getElementById('fullscreen');
+
     bntFullScreen.addEventListener('click', function () {
         isFullScreen = !isFullScreen;
         this.innerHTML = isFullScreen ? '<img src="img/normalScreen.png">' : '<img src="img/fullScreen.png">';
         isFullScreen ? enterFullscreen(fullscreen) : exitFullscreen(fullscreen);
-    })
+    });
 
     bntFullScreen.addEventListener('keydown', function (e) {
         e.preventDefault();
@@ -42,33 +54,48 @@ function fullScreen() {
 }
 
 
+/**
+ * Requests to enter fullscreen mode on the provided element.
+ * Supports various browser-specific fullscreen methods.
+ * 
+ * @param {HTMLElement} element - The HTML element to display in fullscreen mode.
+ */
 function enterFullscreen(element) {
     if (element.requestFullscreen) {
         element.requestFullscreen();
-    } else if (element.msRequestFullscreen) {  
+    } else if (element.msRequestFullscreen) {  // For IE11
         element.msRequestFullscreen();
-    } else if (element.webkitRequestFullscreen) { 
+    } else if (element.webkitRequestFullscreen) { // For Safari
         element.webkitRequestFullscreen();
     }
 }
 
 
+/**
+ * Exits fullscreen mode by calling the appropriate method depending on the browser.
+ */
 function exitFullscreen() {
     if (document.exitFullscreen) {
         document.exitFullscreen();
-    } else if (document.webkitExitFullscreen) {
+    } else if (document.webkitExitFullscreen) { // For Safari
         document.webkitExitFullscreen();
     }
 }
 
 
+/**
+ * Starts the game by initializing game elements, updating the sound button, 
+ * hiding the start view, making the navigation bar visible, and setting up the world and controls.
+ */
 function startGames() {
     initialGames();
     document.getElementById('btn_sound').innerHTML = '<img src="img/pngwing.com.png"/> ';
     document.getElementById('start_view').style.display = 'none';
     document.getElementById('nav_bar_button').style.visibility = 'initial';
+
     const canvas = document.getElementById('canvas');
     scaleCanvas(canvas);
+
     world = new World(canvas, keyboard);
     checkIsWin();
     checkIsGameOver();
@@ -76,12 +103,18 @@ function startGames() {
 }
 
 
+/**
+ * Sets up the pause button functionality, allowing the game to pause and resume. 
+ * Toggles between pause and play icons and controls game sounds accordingly.
+ */
 function setupPauseButton() {
     const buttonPause = document.getElementById('btn_pause');
+
     buttonPause.addEventListener('click', function () {
         world.isGamePause = !world.isGamePause;
         this.innerHTML = world.isGamePause ? '<img src="img/Daco_1000848.png"/>' : '<img src="img/Daco_4414172.png"/> ';
-        if(world.isGamePause) {
+
+        if (world.isGamePause) {
             world.sound.stopAllSoundsEndLevel();
         } else if (!world.isMuted) {
             soundsPlay();
@@ -94,8 +127,13 @@ function setupPauseButton() {
 }
 
 
+/**
+ * Sets up the sound toggle button. Mutes or unmutes the game sounds based on user interaction,
+ * switching between sound on and off icons.
+ */
 function soundOffOnButton() {
     const buttonSound = document.getElementById('btn_sound');
+
     buttonSound.addEventListener('click', function () {
         world.isMuted = !world.isMuted;
         this.innerHTML = world.isMuted ? '<img src="img/pngwing.com (off).png"/>' : '<img src="img/pngwing.com.png"/> ';
@@ -108,6 +146,10 @@ function soundOffOnButton() {
 }
 
 
+/**
+ * Plays the appropriate game sounds based on the game state (level or boss music).
+ * Ensures sounds only play if the game is not muted.
+ */
 function soundsPlay() {
     if (!world.isMuted) {
         world.sound.levelSound.play();
@@ -121,6 +163,9 @@ function soundsPlay() {
 }
 
 
+/**
+ * Restarts the game, stops all game sounds, resets the win or game over state, and hides the end-game views.
+ */
 function tryAgain() {
     world.sound.stopAllSounds();
     startGames();
@@ -131,6 +176,9 @@ function tryAgain() {
 }
 
 
+/**
+ * Checks if the game is over at regular intervals. If the game is over, it displays the game over screen after a short delay.
+ */
 function checkIsGameOver() {
     const gameOverCheck = setInterval(() => {
         if (world.isGameOver) {
@@ -143,18 +191,26 @@ function checkIsGameOver() {
 }
 
 
+/**
+ * Checks if the player has won the game at regular intervals. If the player wins, it displays the win screen after a short delay.
+ */
 function checkIsWin() {
     const tryAgainInterval = setInterval(() => {
         if (world.youWin) {
             clearInterval(tryAgainInterval);
             setTimeout(() => {
                 document.getElementById('you_win').style.display = "block";
-            }, 2000)
+            }, 2000);
         }
     }, 200);
 }
 
 
+/**
+ * Scales the canvas to fit the device's pixel ratio, ensuring high-quality rendering on various devices.
+ * 
+ * @param {HTMLCanvasElement} canvas - The canvas element to scale.
+ */
 function scaleCanvas(canvas) {
     const ctx = canvas.getContext('2d');
     const dpr = window.devicePixelRatio || 1;
--- models/world.class.js
+++ models/world.class.js
@@ -39,11 +39,19 @@ class World {
     }
 
 
+    /**
+     * Sets the world for the character.
+     */
     setWorld() {
         this.character.world = this;
     }
 
 
+    /**
+     * Draws all elements of the game on the canvas. This includes clearing the canvas,
+     * drawing background objects, characters, enemies, miscellaneous objects, status bars,
+     * and requesting the next frame for animation.
+     */
     draw() {
         this.clearCanvas();
         this.drawBackgroundObjects();
@@ -56,28 +64,44 @@ class World {
     }
 
 
+    /**
+     * Clears the canvas and applies the camera translation.
+     */
     clearCanvas() {
         this.ctx.clearRect(0, 0, this.canvas.height, this.canvas.width);
         this.ctx.translate(this.camera_x, 0);
     }
 
 
+    /**
+     * Resets the camera translation after drawing the elements.
+     */
     resetCameraTranslation() {
         this.ctx.translate(-this.camera_x, 0);
     }
+    
 
-
+    /**
+     * Draws all background objects on the canvas by adding them to the map.
+     */
     drawBackgroundObjects() {
         this.addObjectsToMap(this.level.backgroundObjects);
     }
 
 
+    /**
+     * Draws the character and all enemies on the canvas by adding them to the map.
+     */
     drawCharacterAndEnemies() {
         this.addToMap(this.character);
         this.addObjectsToMap(this.level.enemies);
     }
 
 
+    /**
+     * Draws miscellaneous objects on the canvas. These objects include coins, poison bottles, thrown poisons, bubbles, barriers, and the end boss (if available).
+     * Certain objects are only drawn if the game is not paused.
+     */
     drawMiscellaneousObjects() {
         if (!this.isGamePause) {
             this.addObjectsToMap(this.level.coin);
@@ -92,6 +116,10 @@ class World {
     }
 
 
+    /**
+     * Draws status bars (character status, boss status, coin, and poison bars) by adding them to the map.
+     * Also resets and applies camera translation for status bars.
+     */
     drawStatusBars() {
         this.ctx.translate(-this.camera_x, 0);
         this.addToMap(this.statusBar);
@@ -104,6 +132,9 @@ class World {
     }
 
 
+    /**
+     * Draws the final background if needed by adding it to the map.
+     */
     drawFinalBackgroundIfNeeded() {
         if (this.finalBackground) {
             this.addToMap(this.finalBackground);
@@ -111,6 +142,10 @@ class World {
     }
 
 
+    /**
+     * Requests the next animation frame for continuous rendering of the game. 
+     * Uses requestAnimationFrame to call the draw method recursively.
+     */
     requestNextFrame() {
         let self = this;
         requestAnimationFrame(function () {
@@ -119,6 +154,10 @@ class World {
     }
 
 
+    /**
+ * Triggers the arrival of the end boss when the character reaches a specific position (3200).
+ * Spawns the end boss, plays sound, and sets up a check to monitor the end of the level.
+ */
     endBossArrival() {
         let bossSpawned = false;
         const spawnBoss = setInterval(() => {
@@ -128,6 +167,8 @@ class World {
                 bossSpawned = true;
                 this.finalSound();
                 clearInterval(spawnBoss);
+
+                // Check for level end every 200ms
                 this.checkEnd = setInterval(() => {
                     this.checkEndLevel();
                 }, 200);
@@ -136,6 +177,10 @@ class World {
     }
 
 
+    /**
+     * Plays final sound effects when the end boss spawns, including laughter and boss music.
+     * Stops background sounds unless the game is muted.
+     */
     finalSound() {
         if (!this.isMuted) {
             this.sound.adventureTheme.pause();
@@ -145,6 +190,9 @@ class World {
     }
 
 
+    /**
+     * Runs the game loop to check for collisions, throw poison, and play level sounds every 200ms.
+     */
     run() {
         setInterval(() => {
             this.checkCollisions();
@@ -154,6 +202,10 @@ class World {
     }
 
 
+    /**
+     * 
+     * Plays the level background music if the game is not paused, the boss has not appeared, the game is not over, and sound is not muted.
+     */
     levelSoundPlay() {
         if (!this.isGamePause && !this.endBoss && !this.isGameOver && !this.isMuted) {
             this.sound.levelSound.play();
@@ -162,6 +214,11 @@ class World {
     }
 
 
+    /**
+     * Checks if the level has ended by verifying if the end boss is defeated.
+     * If the boss is defeated, triggers the final background, stops all sounds, and plays the victory sound.
+     * Sets the `youWin` flag to true and stops checking for level end.
+     */
     checkEndLevel() {
         if (this.endBoss.endLevel) {
             this.finalBackground = new FinalBackground();
@@ -175,6 +232,10 @@ class World {
     }
 
 
+    /**
+     * Checks for all types of collisions between the character and enemies, bosses, bottles, coins, and attacks.
+     * Handles collision detection for various game objects.
+     */
     checkCollisions() {
         this.checkCollisionsEmemies();
         this.checkCollisionsBoss();
@@ -186,12 +247,20 @@ class World {
     }
 
 
+    /**
+     * Handles what happens when the character collides with an enemy, boss, or other harmful entity.
+     * Reduces the character's health and updates the status bar.
+     */
     checkIsColliding() {
         this.character.hitCharacter();
         this.statusBar.setPercentage(this.character.energy);
     }
 
 
+    /**
+     * Checks for collisions between the character and regular enemies.
+     * If a collision is detected and the enemy is not dead, it triggers the character's hit function.
+     */
     checkCollisionsEmemies() {
         this.level.enemies.forEach((enemy) => {
             if (!enemy.isDead) {
@@ -199,10 +268,15 @@ class World {
                     this.checkIsColliding();
                 }
             }
-        })
+        });
     }
 
 
+    /**
+     * Checks for collisions between the character and poison bottles.
+     * If a collision occurs, the character picks up the bottle, and the poison bar is updated.
+     * Plays a drinking sound if the game is not muted.
+     */
     checkCollisionsBottle() {
         this.poisonBar.setPercentage(this.poisonCount);
         this.level.poisonButtle.forEach((bottle) => {
@@ -212,10 +286,15 @@ class World {
                     this.sound.drinkingPoison.play();
                 }
             }
-        })
+        });
     }
 
 
+    /**
+     * Checks for collisions between the character and coins.
+     * If a coin is collected, the character's coin count increases, and the coin bar is updated.
+     * Plays a coin collection sound if the game is not muted.
+     */
     checkCollisionsCoin() {
         this.level.coin.forEach((coin) => {
             if (this.character.isCollidingPoison(coin)) {
@@ -229,6 +308,10 @@ class World {
     }
 
 
+    /**
+     * Checks for collisions between the character and the end boss.
+     * If a collision is detected and the boss is not dead, the character's hit function is triggered.
+     */
     checkCollisionsBoss() {
         if (this.endBoss && !this.endBoss.isDead) {
             const enemy = this.endBoss;
@@ -239,6 +322,10 @@ class World {
     }
 
 
+    /**
+     * Checks for collisions between the character and bubblefish bubbles.
+     * Handles collision detection for both poisons and bubbles against bubblefish enemies.
+     */
     checkCollisionsBubblefishBubble() {
         this.level.enemies.forEach((enemy) => {
             this.checkPoisonCollisions(enemy);
@@ -247,6 +334,12 @@ class World {
     }
 
 
+    /**
+     * Checks for collisions between thrown poisons and enemies.
+     * If a collision is detected, the poison collision handler is triggered.
+     * 
+     * @param {Object} enemy - The enemy to check collision against.
+     */
     checkPoisonCollisions(enemy) {
         this.throwPoisons.forEach((trowPoison) => {
             if (trowPoison.isCollidingBubbleBossFish(enemy)) {
@@ -256,6 +349,12 @@ class World {
     }
 
 
+    /**
+     * Checks for collisions between bubbles and bubblefish enemies (Green or Red).
+     * If a collision is detected, the bubble collision handler is triggered.
+     * 
+     * @param {Object} enemy - The enemy to check collision against.
+     */
     checkBubbleCollisions(enemy) {
         if (enemy instanceof GreenBubbleFish || enemy instanceof RedBubbleFish) {
             this.throwBubble.forEach((bubble) => {
@@ -267,6 +366,13 @@ class World {
     }
 
 
+    /**
+     * Handles the collision between thrown poison and an enemy.
+     * Reduces the enemy's health and checks if the enemy is defeated by the poison attack.
+     * 
+     * @param {Object} trowPoison - The poison object that hit the enemy.
+     * @param {Object} enemy - The enemy hit by the poison.
+     */
     handlePoisonCollision(trowPoison, enemy) {
         this.downBubblePoison(trowPoison);
         enemy.hitEnemies();
@@ -274,6 +380,13 @@ class World {
     }
 
 
+    /**
+     * Handles the collision between a bubble and an enemy.
+     * Reduces the enemy's health and checks if the enemy is defeated by the bubble attack.
+     * 
+     * @param {Object} bubble - The bubble object that hit the enemy.
+     * @param {Object} enemy - The enemy hit by the bubble.
+     */
     handleBubbleCollision(bubble, enemy) {
         this.downBubble(bubble);
         enemy.hitEnemies();
@@ -281,6 +394,10 @@ class World {
     }
 
 
+    /**
+     * Checks for collisions between bubbles and jellyfish enemies.
+     * Plays sound and removes the enemy if defeated by the bubble attack.
+     */
     checkCollisionsJellyfishBubble() {
         this.level.enemies.forEach((enemy) => {
             if (enemy instanceof JellyFish) {
@@ -299,6 +416,10 @@ class World {
     }
 
 
+    /**
+     * Checks for the boss being attacked by poison or bubble and processes the attack.
+     * If the boss is hit, it reduces the boss's health and plays appropriate sounds.
+     */
     checkCollisionsBossAttack() {
         if (this.endBoss) {
             const enemy = this.endBoss;
@@ -308,6 +429,12 @@ class World {
     }
 
 
+    /**
+     * Checks if the boss is hit by a thrown poison.
+     * Reduces the boss's health and updates the boss's status bar.
+     * 
+     * @param {Object} enemy - The boss to check for collisions.
+     */
     checkBossPoisonAttack(enemy) {
         this.throwPoisons.forEach((trowPoison) => {
             if (trowPoison.isCollidingBubbleBossFish(enemy)) {
@@ -327,6 +454,12 @@ class World {
     }
 
 
+    /**
+     * Checks if the boss is hit by a thrown bubble.
+     * Reduces the boss's health and updates the boss's status bar.
+     * 
+     * @param {Object} enemy - The boss to check for collisions.
+     */
     checkBossBubbleAttack(enemy) {
         this.throwBubble.forEach((bubble) => {
             if (bubble.isCollidingBubbleBossFish(enemy)) {
@@ -342,10 +475,16 @@ class World {
                     enemy.playEndBossIsDead();
                 }
             }
-        })
+        });
     }
 
 
+    /**
+     * Handles enemy hit by poison attack, reducing their health.
+     * Plays sound and disables the enemy if their health reaches zero.
+     * 
+     * @param {Object} enemy - The enemy that was hit.
+     */
     checkHitEnemiesPoisonAttack(enemy) {
         enemy.energyEnemie -= 100;
         if (!this.isMuted) {
@@ -360,6 +499,12 @@ class World {
     }
 
 
+    /**
+     * Handles enemy hit by bubble attack, reducing their health.
+     * Plays sound and disables the enemy if their health reaches zero.
+     * 
+     * @param {Object} enemy - The enemy that was hit.
+     */
     checkHitEnemiesBubbleAttack(enemy) {
         if (enemy instanceof GreenBubbleFish) {
             enemy.energyEnemie -= 100;
@@ -379,6 +524,10 @@ class World {
     }
 
 
+    /**
+     * Handles the transition state of bubblefish enemies.
+     * Runs at an interval of 200ms and triggers the transition for green and red bubblefish if they are alive.
+     */
     bubbleFishTransition() {
         setInterval(() => {
             this.level.enemies.forEach((fish) => {
@@ -390,6 +539,10 @@ class World {
     }
 
 
+    /**
+     * Throws either poison or bubble attacks when the spacebar is pressed, if the game is not paused.
+     * Plays appropriate sound effects based on the action.
+     */
     trowPoison() {
         if (!this.isGamePause) {
             if (this.keyboard.SPACE) {
@@ -406,6 +559,10 @@ class World {
     }
 
 
+    /**
+     * Throws a bubble attack and adds it to the array of thrown bubbles.
+     * Automatically removes the bubble after 3800ms if the game is not paused.
+     */
     trowBubbleAttack() {
         this.character.blowBubble(this.character.IMAGES_BUBBLE);
         let bubble = new BubbleAttack(this.character.x + 100, this.character.y);
@@ -418,6 +575,10 @@ class World {
     }
 
 
+    /**
+     * Throws a poison attack and adds it to the array of thrown poisons.
+     * Reduces the poison count and removes the poison after 4000ms if the game is not paused.
+     */
     trowPoisonAttack() {
         this.character.blowBubble(this.character.IMAGES_BUBBLE_POISON);
         let poison = new PoisonAttack(this.character.x + 100, this.character.y);
@@ -431,6 +592,11 @@ class World {
     }
 
 
+    /**
+     * Adds poison to the character's inventory and removes the poison bottle from the level.
+     * 
+     * @param {Object} buttle - The poison bottle to be added.
+     */
     addPoison(buttle) {
         if (this.poisonCount <= 4) {
             this.level.poisonButtle = this.level.poisonButtle.filter((item) => item !== buttle);
@@ -439,6 +605,9 @@ class World {
     }
 
 
+    /**
+     * Adds a coin to the character's inventory and removes it from the level.
+     */
     addCoin() {
         if (this.coinCount <= 9) {
             this.coinCount++;
@@ -447,6 +616,11 @@ class World {
     }
 
 
+    /**
+     * Adds multiple objects to the map by calling `addToMap` for each object.
+     * 
+     * @param {Array} objects - The array of objects to be added to the map.
+     */
     addObjectsToMap(objects) {
         objects.forEach(o => {
             this.addToMap(o);
@@ -454,6 +628,11 @@ class World {
     }
 
 
+    /**
+     * Draws the object on the canvas. If the object has the `otherDirection` flag, it flips the image.
+     * 
+     * @param {Object} parameter - The object to be drawn.
+     */
     addToMap(parameter) {
         if (parameter.otherDirection) {
             this.flipImage(parameter);
@@ -461,11 +640,17 @@ class World {
 
         parameter.draw(this.ctx);
         if (parameter.otherDirection) {
-            this.flipImageBack(parameter,)
+            this.flipImageBack(parameter);
         }
     }
 
 
+    /**
+     * Flips the image of a given parameter horizontally before drawing it on the canvas.
+     * Saves the current context, translates and scales the image.
+     * 
+     * @param {Object} parameter - The object whose image will be flipped.
+     */
     flipImage(parameter) {
         this.ctx.save();
         this.ctx.translate(parameter.width, 0);
@@ -474,34 +659,68 @@ class World {
     }
 
 
+    /**
+     * Restores the image to its original orientation after drawing.
+     * Reverses the x position and restores the canvas context.
+     * 
+     * @param {Object} parameter - The object whose image will be restored.
+     */
     flipImageBack(parameter) {
         parameter.x = parameter.x * -1;
         this.ctx.restore();
     }
 
 
+    /**
+     * Checks if the given enemy is a GreenBubbleFish and is within 550 units of the character's position.
+     * 
+     * @param {Object} fish - The fish enemy to check.
+     * @returns {boolean} - Returns true if the enemy is a GreenBubbleFish and within range.
+     */
     isGreenBubbleFish(fish) {
         return fish.x - this.character.x < 550 && fish instanceof GreenBubbleFish;
     }
 
 
+    /**
+     * Checks if the given enemy is a RedBubbleFish and is within 450 units of the character's position.
+     * 
+     * @param {Object} fish - The fish enemy to check.
+     * @returns {boolean} - Returns true if the enemy is a RedBubbleFish and within range.
+     */
     isRedBubbleFish(fish) {
         return fish.x - this.character.x < 450 && fish instanceof RedBubbleFish;
     }
 
 
+    /**
+     * Disables an enemy by removing it from the game's level after a 1000ms delay.
+     * The enemy is removed from the `level.enemies` array.
+     * 
+     * @param {Object} enemy - The enemy to disable and remove.
+     */
     enemyDisable(enemy) {
         setTimeout(() => {
             this.level.enemies = this.level.enemies.filter((item) => item !== enemy);
-        }, 1000)
+        }, 1000);
     }
 
 
+    /**
+     * Removes a thrown poison object from the array of active poisons.
+     * 
+     * @param {Object} trowPoison - The poison object to be removed.
+     */
     downBubblePoison(trowPoison) {
         this.throwPoisons = this.throwPoisons.filter((item) => item !== trowPoison);
     }
 
-
+    
+    /**
+     * Removes a thrown bubble object from the array of active bubbles.
+     * 
+     * @param {Object} bubble - The bubble object to be removed.
+     */
     downBubble(bubble) {
         this.throwBubble = this.throwBubble.filter((item) => item !== bubble);
     }
